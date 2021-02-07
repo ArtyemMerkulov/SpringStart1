0fa793b960adfd0cf89589d057d1e74d78e11347
ArtyemMerkulov <artyemmerkylov@gmail.com>
2021-02-07 15:19:58 +0700

    Homework 2

---
+++ lesson-02/src/main/java/ru/geekbrains/server/auth/AuthServiceJdbcImpl.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.server.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.geekbrains.server.User;
+import ru.geekbrains.server.persistance.UserRepository;
+
+import java.sql.SQLException;
+
+@Service
+public class AuthServiceJdbcImpl implements AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean authUser(User user) {
+        try {
+            User usr = userRepository.findByLogin(user.getLogin());
+            return usr.getId() > 0 && usr.getPassword().equals(user.getPassword());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
